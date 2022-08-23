0ece5fe34d82256c42ff29d9707fa5204a0f9a51
nicolashouriet <nicolas.houriet@protonmail.ch>
2022-08-24 01:40:16 +0200

    init

---
+++ Data/PizzaService.cs
@@ -0,0 +1,10 @@
+namespace BlazingPizza.Data;
+
+public class PizzaService
+{
+    public async Task<Pizza[]> GetPizzasAsync()
+    {
+        return Array.Empty<Pizza>();
+    // Call your data access technology here
+    }
+}
